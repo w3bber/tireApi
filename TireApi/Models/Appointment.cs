690cd80996ec610d246c602a3291b7683b04c720
Alexander <alexandr.shiryaev.04@gmail.com>
2025-02-22 14:33:31 +0300

    Добавлены все основые модели(сущности)

---
+++ TireApi/Models/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TireApi.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        // Внешние ключи
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; } 
+
+        public int CarId { get; set; }
+        [ForeignKey("CarId")]
+        public Car Car { get; set; }
+        public ICollection<AppointmentServiceType> AppointmentServiceTypes { get; set; }
+    }
+}
